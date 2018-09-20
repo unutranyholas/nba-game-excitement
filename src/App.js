abf112afdd600f6746a0bf5e8b1e42bf67070497
Ihar Trafimovich <unutranyholas@gmail.com>
2018-09-20 21:20:01 +0300

    Adapt API changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./App.css";
 import logos from "./Logos";
 
-const serverUrl = "https://nba-game-excitement.herokuapp.com";
+// const serverUrl = "https://nba-game-excitement.herokuapp.com";
+const serverUrl = "http://localhost:5000";
 
 const GamePreview = (props) => {
   const {game} = props;
-  const {hTeam, vTeam, gameExcitement} = game;
+  const {gameId, date, gameData, gameExcitement} = game;
+  const {vTeam, hTeam} = gameData;
+
   return (
     <div style={{
       display: "grid",
@@ -15,10 +18,14 @@ const GamePreview = (props) => {
     }}>
       <div>{vTeam.triCode}</div>
       <div>{hTeam.triCode}</div>
+      <div>{gameId}</div>
+      <div>
+        <img alt={vTeam.triCode} src={logos[`${vTeam.triCode}_logo`]} width={100} height={100} />
+      </div>
+      <div>
+        <img alt={vTeam.triCode} src={logos[`${hTeam.triCode}_logo`]} width={100} height={100} />
+      </div>
       <div>{gameExcitement}</div>
-      <div><img src={logos[`${vTeam.triCode}_logo`]} width={100} height={100} /></div>
-      <div><img src={logos[`${hTeam.triCode}_logo`]} width={100} height={100} /></div>
-      <div></div>
     </div>
   );
 };
@@ -30,13 +37,15 @@ class App extends React.Component {
   };
 
   async componentDidMount() {
-    const games = await fetch(`${serverUrl}/games/20171017`);
+    const games = await fetch(`${serverUrl}/games/20171018`);
     const gamesJson = await games.json();
     this.setState({data: gamesJson, loaded: true});
   }
 
   render() {
     const {data, loaded} = this.state;
+
+    console.log(data);
     return (
       <div>
         {
