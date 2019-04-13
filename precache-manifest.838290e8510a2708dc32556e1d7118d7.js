4f5faa78a6ee2f8c4abee2593b7f134e28bbb761
Ihar Trafimovich <unutranyholas@gmail.com>
2019-04-13 20:57:10 +0300

    Updates

---
+++ precache-manifest.838290e8510a2708dc32556e1d7118d7.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = [
+  {
+    "revision": "62abcd240157bf530339",
+    "url": "/static/css/main.c1e7d931.chunk.css"
+  },
+  {
+    "revision": "62abcd240157bf530339",
+    "url": "/static/js/main.62abcd24.chunk.js"
+  },
+  {
+    "revision": "ac499969143a8d4e5e27",
+    "url": "/static/js/1.ac499969.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c962c22c56eb9ce548e6cfb297bbf9f7",
+    "url": "/static/media/Formula1.c962c22c.svg"
+  },
+  {
+    "revision": "ea7059e1269af964937226b5dd391f2b",
+    "url": "/static/media/Formula2.ea7059e1.svg"
+  },
+  {
+    "revision": "c82adca5341e456e7cbc8fac827ac0a6",
+    "url": "/static/media/ATL_logo.c82adca5.svg"
+  },
+  {
+    "revision": "ae41dbf47537a7764fe19669235e97e5",
+    "url": "/static/media/BKN_logo.ae41dbf4.svg"
+  },
+  {
+    "revision": "d1bb25d3795ebaa99512f8a1f538a65d",
+    "url": "/static/media/BOS_logo.d1bb25d3.svg"
+  },
+  {
+    "revision": "59115b4e776877a9557a2126a78a1fca",
+    "url": "/static/media/CHI_logo.59115b4e.svg"
+  },
+  {
+    "revision": "3d4651a1a07a93e132a70d30ffa8f49f",
+    "url": "/static/media/CHA_logo.3d4651a1.svg"
+  },
+  {
+    "revision": "420882b43d5db710bea4b70020f8f35d",
+    "url": "/static/media/CLE_logo.420882b4.svg"
+  },
+  {
+    "revision": "d5f1cc70826d9e420849e341c5724bc5",
+    "url": "/static/media/DAL_logo.d5f1cc70.svg"
+  },
+  {
+    "revision": "9b5d2bd73e239f637125394ee430c9da",
+    "url": "/static/media/DEN_logo.9b5d2bd7.svg"
+  },
+  {
+    "revision": "8782fe91be897dd94b4fc9776281bb3b",
+    "url": "/static/media/DET_logo.8782fe91.svg"
+  },
+  {
+    "revision": "f48e8cee4eb5884a86835c916a741655",
+    "url": "/static/media/GSW_logo.f48e8cee.svg"
+  },
+  {
+    "revision": "82237524f51a3ca92d60d638337a3f34",
+    "url": "/static/media/HOU_logo.82237524.svg"
+  },
+  {
+    "revision": "c93344f80f2258799ce8a8f09a033d6d",
+    "url": "/static/media/IND_logo.c93344f8.svg"
+  },
+  {
+    "revision": "0a4c0ac14c556c7642f5d1fca7f6aa2f",
+    "url": "/static/media/LAC_logo.0a4c0ac1.svg"
+  },
+  {
+    "revision": "a5f2ab1202c7e1470d68d35ba5b83255",
+    "url": "/static/media/LAL_logo.a5f2ab12.svg"
+  },
+  {
+    "revision": "024dd1ec8dc71b94e73095e25bf14215",
+    "url": "/static/media/MEM_logo.024dd1ec.svg"
+  },
+  {
+    "revision": "ab1a9a53dc1cdd53294876deea76c4eb",
+    "url": "/static/media/MIA_logo.ab1a9a53.svg"
+  },
+  {
+    "revision": "d7ca61532d6a6eb1469aa425a5234686",
+    "url": "/static/media/MIL_logo.d7ca6153.svg"
+  },
+  {
+    "revision": "d050d0b5ec98320c8e3aa217b294d65c",
+    "url": "/static/media/MIN_logo.d050d0b5.svg"
+  },
+  {
+    "revision": "840cf799a3c57be8fdf1e10e213fa347",
+    "url": "/static/media/NOP_logo.840cf799.svg"
+  },
+  {
+    "revision": "530a0f044d7048b6adb744ecb3c3057a",
+    "url": "/static/media/NYK_logo.530a0f04.svg"
+  },
+  {
+    "revision": "3639f258f077c511bdf491428c5b0671",
+    "url": "/static/media/OKC_logo.3639f258.svg"
+  },
+  {
+    "revision": "c4fdb54be919080749dfb0220d3a2b86",
+    "url": "/static/media/ORL_logo.c4fdb54b.svg"
+  },
+  {
+    "revision": "53718eeb41b40f14913f2a3420e7d5c6",
+    "url": "/static/media/PHI_logo.53718eeb.svg"
+  },
+  {
+    "revision": "882efa626154883d6d029dd075244ac1",
+    "url": "/static/media/PHX_logo.882efa62.svg"
+  },
+  {
+    "revision": "3eab190e6cfe75e35693bae09758615f",
+    "url": "/static/media/POR_logo.3eab190e.svg"
+  },
+  {
+    "revision": "2ad59a5c43454f08c20f2d92c6582d44",
+    "url": "/static/media/SAC_logo.2ad59a5c.svg"
+  },
+  {
+    "revision": "1eab7512b07b6ac8da7584673cad1d38",
+    "url": "/static/media/SAS_logo.1eab7512.svg"
+  },
+  {
+    "revision": "e307cbe80e223c498ff28a5b57987428",
+    "url": "/static/media/TOR_logo.e307cbe8.svg"
+  },
+  {
+    "revision": "f51f5614b08dae2a73741602f5dbd7c3",
+    "url": "/static/media/UTA_logo.f51f5614.svg"
+  },
+  {
+    "revision": "073ad0cbae047457d3879fa1f177d726",
+    "url": "/static/media/WAS_logo.073ad0cb.svg"
+  },
+  {
+    "revision": "55c0f977f58b22210fa1dc4b1e198329",
+    "url": "/static/media/NBA_logo.55c0f977.svg"
+  },
+  {
+    "revision": "5fca75b04ac19f21c8191e4863a0bbf8",
+    "url": "/index.html"
+  }
+];
